acc5b359f26c1873ea6195edc187ad81f9054aeb
vaalsilva <44147868+vaalsilva@users.noreply.github.com>
2018-11-01 17:06:22 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install apache2 -y
-sudo systemctl start apache2
+
+# Criar usuário e grupo de administração
+groupadd -g 1001 oinstall
+useradd -u 1001 -g oinstall oracle
+
+# Criar diretórios onde os arquivos serão instalados
+mkdir -p /u01/app/oracle/middleware && mkdir -p /u01/app/oracle/config/domains && mkdir -p /u01/app/oracle/config/applications
+chown -R oracle:oinstall /u01
+chmod -R 775 /u01/
